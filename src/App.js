7a2e451f83b6015be0b65a9952b2cd50d29e27dd
Egor Kochkin <egurt96@yandex.ru>
2020-11-06 17:37:19 +0300

    add chat component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import SideBar from './components/sidebar/sidebar'
-
+import Chat from './components/chat/chat'
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
         <SideBar />
+        <Chat />
       </div>
     </div>
   );
