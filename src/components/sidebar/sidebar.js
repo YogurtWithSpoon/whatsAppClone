606fd64ba941de3a696b83d90dc5a0fec5909c6f
Egor Kochkin <egurt96@yandex.ru>
2020-11-06 15:52:25 +0300

    create sidebar component

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./sidebar.scss";
+
+//material ui 
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import SearchIcon from '@material-ui/icons/Search';
+
+//components
+import SideBarChat from '../sidebarchat/sidebarchat'
 
 function sidebar() {
   return (
-    <div>
-      <h1>sidebar</h1>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__header_right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__search__container">
+          <SearchIcon />
+          <input placeholder="Search or start new chat" type="text"/>
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SideBarChat />
+        <SideBarChat />
+        <SideBarChat />
+      </div>
     </div>
-  )
+  );
 }
 
-export default sidebar
+export default sidebar;
