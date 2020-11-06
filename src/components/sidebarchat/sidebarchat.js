e93acbba26d653fa3946e35cab27e084a558fba9
Egor Kochkin <egurt96@yandex.ru>
2020-11-06 16:07:40 +0300

    add logic for new chat

--- src/components/sidebarchat/sidebarchat.js
+++ src/components/sidebarchat/sidebarchat.js
@@ -1,23 +1,34 @@
-import React,{useEffect, useState} from 'react'
-import './sidebarchat.scss'
+import React, { useEffect, useState } from "react";
+import "./sidebarchat.scss";
 import { Avatar } from "@material-ui/core";
 
-function Sidebarchat() {
-  const [seed,setSeed] = useState('');
+function Sidebarchat({ addNewChat }) {
+  const [seed, setSeed] = useState("");
 
   useEffect(() => {
-    setSeed(Math.floor(Math.random() * 5000))
-  },[])
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
 
-  return (
+  const createNewChat = () => {
+    const roomName = prompt("Please enter name for chat");
+    if (roomName) {
+      //database actions
+    }
+  };
+
+  return !addNewChat ? (
     <div className="sidebar__chat">
       <Avatar src={`https:/avatars.dicebear.com/api/human/${seed}.svg`} />
-      <div className="sidebarChat__info">
+      <div className="sidebar__chat__info">
         <h2>Room Name</h2>
-        <p>Last Message</p>
+        <p>Last Message...</p>
       </div>
     </div>
-  )
+  ) : (
+    <div className="sidebar__chat" onClick={createNewChat}>
+      <h2>Add new Chat</h2>
+    </div>
+  );
 }
 
-export default Sidebarchat
+export default Sidebarchat;
