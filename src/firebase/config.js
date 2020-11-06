c8490d4ba9813c4c9c83a978347abb9de4f348c3
Egor Kochkin <egurt96@yandex.ru>
2020-11-06 14:36:14 +0300

    init project

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMXQFJQp0vpLc9TRmHqDs32TdgmUL9a6A",
+  authDomain: "whatsappclone-1e65a.firebaseapp.com",
+  databaseURL: "https://whatsappclone-1e65a.firebaseio.com",
+  projectId: "whatsappclone-1e65a",
+  storageBucket: "whatsappclone-1e65a.appspot.com",
+  messagingSenderId: "614954107962",
+  appId: "1:614954107962:web:9884d767d69d2b0082a103"
+};
