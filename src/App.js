a5b2bcfc4dd682cfe495348d1bdab680c6b6c39b
Egor Kochkin <egurt96@yandex.ru>
2020-11-07 20:38:21 +0300

    check auth with username variable condition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.scss';
+import React from 'react';
 import SideBar from './components/sidebar/sidebar'
 import Chat from './components/chat/chat'
+import Login from './components/login/login';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {useStateValue} from './datalayer/stateprovider'
 
 function App() {
+  const [{userName}, dispatch ] = useStateValue();
+  
   return (
     <div className="app">
-      <div className="app__body">
+      {!userName ? (
+        <Login />
+      ) : ( 
+        <div className="app__body">
         <Router>
               <SideBar />
               <Switch>
@@ -16,6 +24,7 @@ function App() {
               </Switch>
         </Router>
       </div>
+      )}
     </div>
   );
 }
