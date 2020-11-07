29e9f808f8a0c5752e0034e2169084de5c5c51f3
Egor Kochkin <egurt96@yandex.ru>
2020-11-07 17:55:59 +0300

    added subscribe for database update and thrown props(id,name) for sidebarchat component

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./sidebar.scss";
 
-//material ui 
+//material ui
 import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 
 //components
-import SideBarChat from '../sidebarchat/sidebarchat'
+import SideBarChat from "../sidebarchat/sidebarchat";
 
-function sidebar() {
+//firebase
+import db from "../../firebase/api";
+
+function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) => {
+      return setRooms(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            data: doc.data(),
+          };
+        })
+      );
+    });
+    return (() => {
+      unsubscribe();
+    })
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -31,16 +51,18 @@ function sidebar() {
       <div className="sidebar__search">
         <div className="sidebar__search__container">
           <SearchIcon />
-          <input placeholder="Search or start new chat" type="text"/>
+          <input placeholder="Search or start new chat" type="text" />
         </div>
       </div>
       <div className="sidebar__chats">
-        <SideBarChat addNewChat/>
-        <SideBarChat />
-        <SideBarChat />
+        <SideBarChat addNewChat />
+        {rooms.map(room => (
+          <SideBarChat key={room.id} id={room.id}
+          name={room.data.name} />
+        ))}
       </div>
     </div>
   );
 }
 
-export default sidebar;
+export default Sidebar;
