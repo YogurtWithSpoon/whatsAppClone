cc6154683774a5ccc9d15906c63ae79380e75782
Egor Kochkin <egurt96@yandex.ru>
2020-11-07 20:40:19 +0300

    last message display and remove link text-decoration

--- src/components/sidebarchat/sidebarchat.js
+++ src/components/sidebarchat/sidebarchat.js
@@ -6,10 +6,23 @@ import {Link} from 'react-router-dom'
 
 function Sidebarchat({ addNewChat, name, id }) {
   const [seed, setSeed] = useState("");
+  const [messages,setMessages] = useState([])
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  useEffect(() => {
+    if(id){
+      db.collection('rooms')
+      .doc(id)
+      .collection('messages')
+      .orderBy('timestamp','desc')
+      .onSnapshot(snapshot => (
+        setMessages(snapshot.docs.map(doc => doc.data()))
+      ))
+    }
+  },[id])
 
   const createNewChat = () => {
     const roomName = prompt("Please enter name for chat");
@@ -25,7 +38,7 @@ function Sidebarchat({ addNewChat, name, id }) {
         <Avatar src={`https:/avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="sidebar__chat__info">
           <h2>{name}</h2>
-          <p>Last Message...</p>
+          <p>{messages[0]?.message}</p>
         </div>
       </div>
     </Link>
