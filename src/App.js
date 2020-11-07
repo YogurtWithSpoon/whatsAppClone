326f80e747b44df2b632ba3d4f116ff97dd256b3
Egor Kochkin <egurt96@yandex.ru>
2020-11-07 17:56:46 +0300

    added react router for redirect to certain id chat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.scss';
 import SideBar from './components/sidebar/sidebar'
 import Chat from './components/chat/chat'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <SideBar />
-        <Chat />
+        <Router>
+              <SideBar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+              </Switch>
+        </Router>
       </div>
     </div>
   );
